6e253ec9c8cbf184dbec223833f7abe3e0be6a1d
maxiamikel19 <maxloversist19@gmail.com>
2025-04-30 09:20:04 -0400

    Inicial commit

---
+++ src/main/java/com/maxiamikel/url_compressor_spring_api/UrlCompressorSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxiamikel.url_compressor_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlCompressorSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlCompressorSpringApiApplication.class, args);
+	}
+
+}
